db8337839b787305ae3971d173b36559dc21688c
Laaraj Moncif <laarajmoncif@gmail.com>
2024-04-28 10:44:30 +0100

    add cart component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,11 +3,15 @@ import { Link, Outlet } from 'react-router-dom';
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
 import './navigation.styles.scss';
 import  {UserContext} from '../../contexts/user.context';
+import  {CartContext} from '../../contexts/cart.context';
 import {signOutUser} from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropDown from '../../components/cart-dropdown/cart-dropdown.component';
 
 
 const Navigation=()=>{
     const {currentUser}=useContext(UserContext);
+    const {isCartOpen}=useContext(CartContext);
 
     return(
         <Fragment>
@@ -21,9 +25,11 @@ const Navigation=()=>{
                     currentUser ?(<span className='nav-link' onClick={signOutUser}> Sign OUT</span>) :
                     (<Link className='nav-link' to='/auth'>SignIn</Link> )
                     }
+                    <CartIcon />
          
                 </div>
-                      </div>
+                {isCartOpen && <CartDropDown  />}
+            </div>
             <Outlet />
         </Fragment>
     )
