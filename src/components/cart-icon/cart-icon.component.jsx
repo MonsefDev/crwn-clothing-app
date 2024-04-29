82d2400abaae9d8c4d6eb4bbed33e679c6e397c0
Laaraj Moncif <laarajmoncif@gmail.com>
2024-04-29 12:11:13 +0100

    add to cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,12 +5,12 @@ import { CartContext } from '../../contexts/cart.context';
 
 
 const CartIcon =()=>{
-    const {isCartOpen,setIsCartOpen}=useContext(CartContext);
+    const {isCartOpen,setIsCartOpen,carCount}=useContext(CartContext);
     const toggleOpenCart=()=>{ setIsCartOpen(!isCartOpen)}
     return(
         <div className='cart-icon-container' onClick={toggleOpenCart}>
             <ShoppingIcon  className='shopping-icon'/>
-            <span className='item-count'>0</span>
+            <span className='item-count'>{carCount}</span>
         </div>
     )
 }
