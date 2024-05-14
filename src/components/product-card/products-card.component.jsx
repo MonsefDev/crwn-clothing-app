718179a05d2fb52b41d98800360c1fb6ebfa2307
Laaraj Moncif <laarajmoncif@gmail.com>
2024-05-14 15:23:06 +0100

    clean code

--- src/components/product-card/products-card.component.jsx
+++ src/components/product-card/products-card.component.jsx
@@ -10,8 +10,6 @@ const ProductCard = ({ product }) => {
   
     const addProductToCart = () => addItemToCart(product);
 
-    console.log('ptod',product)
-  
     return (
       <div className='product-card-container'>
         <img src={imageUrl} alt={`${name}`} />
